489ae2fbab503a14fa9f4b6bc159b4dec702a543
Edmond <Edmond.Shaffer@gmail.com>
2024-06-26 17:43:50 -0700

    Calender component

---
+++ src/tests/Calendar.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Calendar from '../components/Calendar';
+import '@testing-library/jest-dom';
+
+describe('Calendar Component', () => {
+  it('renders weekdays correctly', () => {
+    const { getAllByText } = render(<Calendar />);
+    expect(getAllByText('S').length).toBe(2);
+    expect(getAllByText('M').length).toBe(1);
+    expect(getAllByText('T').length).toBe(2);
+    expect(getAllByText('W').length).toBe(1);
+  });
+
+  test('renders calendar header and CalendarDays component correctly', () => {
+    const { getByText } = render(<Calendar />);
+
+    expect(getByText('Jun 2024')).toBeInTheDocument();
+    expect(getByText('<')).toBeInTheDocument();
+    expect(getByText('>')).toBeInTheDocument();
+  });
+
+  it('clicking next and previous buttons updates current month', () => {
+    const { getByText } = render(<Calendar />);
+
+    // Click the next button ('>')
+    fireEvent.click(getByText('>'));
+    expect(getByText('Jul 2024')).toBeInTheDocument();
+
+    // Click the previous button ('<')
+    fireEvent.click(getByText('<'));
+    expect(getByText('Jun 2024')).toBeInTheDocument();
+  });
+});
